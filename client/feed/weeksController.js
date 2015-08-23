15213e49b609ec44fb5782db4ee58a9869ff5c44
stefanRitter <stefan@stefanritter.com>
2015-08-23 17:40:52 +0100

    style filtering

---
+++ client/feed/weeksController.js
@@ -0,0 +1,31 @@
+angular.module('app').controller('weeksController', ['$http', function ($http) {
+  'use strict';
+
+  var vm = this;
+  vm.weeks = [];
+  vm.orderFilter = 'calendarWeek';
+  vm.orderFilterDirection = false;
+
+  vm.changeFilter = function (filter) {
+    if (vm.orderFilter === filter) {
+      vm.orderFilterDirection = !vm.orderFilterDirection;
+    } else {
+      vm.orderFilter = filter;
+      vm.orderFilterDirection = true;
+    }
+  };
+
+  vm.formatCollectionPath = function (path) {
+    return path.split('/')[5].replace(/-/g,' ');
+  };
+
+  $http
+    .get('/weeks', {})
+    .error(function (err) {
+      console.log('error', err);
+    })
+    .success(function (result) {
+      vm.weeks = result;
+    });
+
+}]);
