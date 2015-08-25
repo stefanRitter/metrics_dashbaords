7a778e6a0821a2189d075df231a397d46e6b2a6a
stefanRitter <stefan@stefanritter.com>
2015-08-25 07:53:42 +0100

    return to previous path after login

--- client/auth/currentUser.js
+++ client/auth/currentUser.js
@@ -1,18 +1,23 @@
 angular.module('app').factory('currentUser', ['$location', '$http', '$rootScope', function ($location, $http, $rootScope) {
   'use strict';
 
-  var currentUser;
+  var currentUser,
+      previousPath;
 
   function login (user) {
     currentUser = user;
     $rootScope.$emit('userLoggedIn');
-    $location.path('/collections');
+    $location.path(previousPath);
   }
 
   $http
     .get('/session', {})
     .error(function () {
       console.log('user not authenticated');
+      previousPath = $location.path();
+      if (previousPath === '/' || previousPath === '/login') {
+        previousPath = '/collections';
+      }
       $location.path('/login');
     })
     .success(login);
