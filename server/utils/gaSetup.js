5091b924a3960cf12fa4ec570e1c472c324d3fc1
stefanRitter <stefan@stefanritter.com>
2015-11-29 16:12:43 +0100

    update start date and names

--- server/utils/gaSetup.js
+++ server/utils/gaSetup.js
@@ -13,7 +13,7 @@ var COLLECTIONS_VIEW_ID = 'ga:106745667';
 var ALL_DATA_VIEW_ID = 'ga:106585530';
 var LOGGEDIN_VIEW_ID = 'ga:106761745';
 
-var START_DATE = '2015-08-24';
+var START_DATE = '2015-11-30';
 var END_DATE = '2016-01-01';
 
 var authClient = new JWT(
